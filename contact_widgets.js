04f71e1b61c9e883eb91e2c8c6d0908ae9b548fe
Lerry William <ghostspell89@yahoo.com>
2019-09-26 10:55:54 +0800

    Create contact_widgets.js

---
+++ contact_widgets.js
@@ -0,0 +1,14 @@
+(function () {var options = {
+whatsapp: "+60198908285",
+telegram: "lerryws",
+email: "lerryws.xyz@outlook.com",
+call_to_action: "Do you need help?",
+button_color: "#6dcc6e",
+position: "right",
+order: "whatsapp,telegram,email",
+};
+var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
